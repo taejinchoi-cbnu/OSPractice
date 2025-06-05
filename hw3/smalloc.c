5c536bf27f7b4b895a3fe56da17a3b90dcc7a5d0
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-06-06 01:14:04 +0900

    과제 진짜 진짜 완료

--- hw3/smalloc.c
+++ hw3/smalloc.c
@@ -60,7 +60,6 @@ void * smalloc (size_t s)
 	}
 	else {
 		// first fit
-
 		smheader_ptr first = NULL;
 		size_t first_chunk = 0;
 
@@ -113,17 +112,11 @@ void * smalloc (size_t s)
 		new_header->next = NULL;
 
 		// connect new memory to free-list
-		if (smlist == NULL) {
-			smlist = new_header;
+		smheader_ptr current = smlist;
+		while (current->next != NULL) {
+			current = current->next;
 		}
-		else {
-			smheader_ptr current = smlist;
-			while (current->next != NULL) {
-				current = current->next;
-			}
-			current->next = new_header;
-		}
-
+		current->next = new_header;
 		// re-find (recurcive)
 		return smalloc(s);
 	}
@@ -160,80 +153,70 @@ void sfree (void * p)
 void * srealloc (void * p, size_t s) 
 {
     // if p is NULL
-	if (p == NULL) return smalloc(s);
-	// if s is 0
-	if (s == 0) {
-		sfree(p);
-		return NULL;
-	}
-	// TODO: 둘 다 아닌 경우 s에 맞는 크기 smlist에서 탐색
-	smheader_ptr origin_header = (smheader_ptr)((char *)p - sizeof(smheader));
-	size_t origin_size = origin_header->size;
-	
-	// case가 3개 s가 작아지는 경우, 같은 경우, 큰 경우
+    if (p == NULL) return smalloc(s);
+    // if s is 0
+    if (s == 0) {
+        sfree(p);
+        return NULL;
+    }
+    
+    smheader_ptr origin_header = (smheader_ptr)((char *)p - sizeof(smheader));
+    size_t origin_size = origin_header->size;
+    
+    // s가 줄어드는 경우
+    if (s < origin_size) {
+        if (origin_size > s + sizeof(smheader)) {
+            smheader_ptr split_header = (smheader_ptr)((char *)origin_header + sizeof(smheader) + s);
+            split_header->size = origin_size - s - sizeof(smheader);
+            split_header->used = 0;
+            split_header->next = origin_header->next; 
+            origin_header->next = split_header;
+            origin_header->size = s;
+            smcoalesce();
+        } else {
+            origin_header->size = s;
+        }
+        return p;
+    }
+    // 같은 경우
+    else if (s == origin_size) {
+        return p;
+    }
+    // s > origin_size
+    else {
+        // 인접한 블록과 병합 시도
+        if (origin_header->next != NULL && 
+            origin_header->next->used == 0 && 
+            origin_size + origin_header->next->size + sizeof(smheader) >= s) {
+            
+            // 병합 가능!
+            origin_header->size = origin_size + sizeof(smheader) + origin_header->next->size;
+            smheader_ptr next_block = origin_header->next;
+            origin_header->next = next_block->next;
 
-	// s가 줄어드는 경우
-	if (s < origin_size) {
-		// header를 포함해서 분할 가능한지 확인 후
-		if (origin_size > s + sizeof(smheader)) {
-			// 가능하다면 split 후 coalesce
-			smheader_ptr split_header = (smheader_ptr)((char *)origin_header + sizeof(smheader) + s);
-			split_header->size = origin_size - s - sizeof(smheader);
-			split_header->used = 0;
-			origin_header->size = s;
-			origin_header->next = split_header;
-			smcoalesce();
-			return p;
-		}
-		else {
-			origin_header->size = s;
-		}
-		return p;
-	}
-	// 같은 경우 아무일도 안 일어남
-	else if (s == origin_size) {
-		return p;
-	}
-	// s > origin_size
-	else {
-		// 인접한 블록으로 병합 시도해보기
-		if (origin_header->next != NULL && origin_header->next->used == 0) {
-			if (origin_size + origin_header->next->size + sizeof(smheader) >= s) {
-				// TODO: 1. 병합 가능한 총 크기를 origin_header->size에 반영
-				origin_header->size = origin_size + sizeof(smheader) + origin_header->next->size;
-            	smheader_ptr next_block = origin_header->next; // 제거될 다음 블록을 임시 저장
-                origin_header->next = next_block->next; // 다음 블록을 건너뛰어 연결
-
-				// TODO: 2. 확장 후 남는 공간이 충분하면 분할하고, 아니면 모두 사용
-				if (origin_header->size > s + sizeof(smheader)) {
-					smheader_ptr split_header = (smheader_ptr)((char *)origin_header + sizeof(smheader) + s);
-					split_header->size = origin_header->size - s - sizeof(smheader);
-					split_header->used = 0;
-					split_header->next = origin_header->next;
-
-					// 원본 node의 크기 수정 (필요 없는 부분은 제거 했으니까)
-					origin_header->size = s;
-
-					// 분할하고 병합
-					smcoalesce();
-				}
-				return p;
-			}
-		}
-		// 인접한 블록으로 병합이 안되면 새로 요청
-		else {
-			// 추가로 요청할 메모리
-			smheader_ptr new_ptr = smalloc(s);
-			// 추가 요청 실패 핸들링
-			if (new_ptr == NULL) return NULL;
-			// p의 데이터를 추가 요청 메모리로 복사 후 기존 메모리 free
-			memcpy(new_ptr, p, origin_size);
-			// 복사 후 기존 메모리 free
-			sfree(p);
-			// 새롭게 할당된 p값 return
-			return new_ptr;
-		}
-	}
+            // 확장 후 남는 공간이 충분하면 분할
+            if (origin_header->size > s + sizeof(smheader)) {
+                smheader_ptr split_header = (smheader_ptr)((char *)origin_header + sizeof(smheader) + s);
+                split_header->size = origin_header->size - s - sizeof(smheader);
+                split_header->used = 0;
+                split_header->next = origin_header->next;
+                origin_header->next = split_header;
+                origin_header->size = s;
+                smcoalesce();
+            }
+            return p;
+        }
+        
+        // 1. 인접 블록이 없거나
+        // 2. 인접 블록이 사용중이거나  
+        // 3. 병합해도 크기가 부족한 경우
+        void* new_ptr = smalloc(s);
+        if (new_ptr == NULL) return NULL;
+        
+        memcpy(new_ptr, p, origin_size);
+        sfree(p);
+        return new_ptr;
+    }
 }
 
 void smcoalesce ()
